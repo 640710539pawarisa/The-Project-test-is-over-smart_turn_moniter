3172ad12e94953ecf23c6489ebd1dc434270439b
pawarisa <rattanathiantho_p@silpakorn.edu>
2025-05-05 17:50:41 +0700

    อัปเดตฟีเจอร์ใหม่ / แก้ไขบั๊ก / เพิ่มไฟล์ ฯลฯ

--- basic10.py
+++ basic10.py
@@ -3,7 +3,7 @@ import cv2
 img = cv2.imread("image/Klage.jpeg")#อ่านภาพเข้ามา
 
 #กำหนดขนาดภาพ
-imgresize = cv2.resize(img, (700,700))#ความกว้าง,ความสูง
+imgresize = cv2.resize(img, (700,700))#ความกว้าง,ความสูง #imgresize คือ ตัวแปรที่สร้างไว้เก็บภาพ
 
 #วาดเส้นตรง
 #line (ภาพ , จุดเริ่มต้น(x , y) , จุดสิ้นสุด(x , y) , สี (BGR), ความหนา) #สีเป็น BGR คือ blue , green , red 
