6483255d3e0cf96a5b2a73587132ecead894b35f
pawarisa <rattanathiantho_p@silpakorn.edu>
2025-05-08 12:26:21 +0700

    แก้ไขโค้ด

--- basic19.py
+++ basic19.py
@@ -3,15 +3,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("image/RGB.png")
+img = cv2.imread("image/rainbow_ball.jpg")
 
-#ตรวจจับกลุ่มวัตถุจากสี
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-lower = np.array([0,0,0])
-upper = np.array([255,255,255])
 
-mask = cv2.inRange(img_hsv, lower, upper)
-
-cv2.imshow("Output", mask)
+#ตรวจจับกลุ่มวัตถุจากสี   
+cv2.imshow("Output", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
