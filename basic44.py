3323c56e0130a5e7e13064c3294d91dd7e01cc8e
pawarisa <rattanathiantho_p@silpakorn.edu>
2025-05-15 19:08:43 +0700

    เพิ่มโค้ด

--- basic44.py
+++ basic44.py
@@ -3,5 +3,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("image/coins.jpg",0)#อ่านภาพ
+img = cv2.imread("image/coinsbritish.jpg",0)#อ่านภาพ
 img =cv2.resize(img,(500,350))#ปรับขนาดภาพ
+
+#ใช้ในการตรวจจับขอบภาพด้วย Laplacian Method
+laplacian = cv2.Laplacian(img,-1)#cv2.Laplacian (array รูปภาพ, ชนืดตัวแปรในอาเรย(ใส่-1))
+
+cv2.imshow("Original",img)#แสดงภาพ, "Original" คือชื่อภาพตัวแรก, img คือภาพที่เราอ่านมา
+cv2.imshow("Laplacian",laplacian)#แสดงภาพ, "Original" คือชื่อภาพตัวแรก , "Laplacian" คือชื่อภาพตัวที่สอง
+
+cv2.waitKey(0)#รอให้ผู้ใช้งานกดปุ่มเพื่อปิดหน้าต่าง
+cv2.destroyAllWindows()#ปิดหน้าต่าง
--- basic45.py
+++ basic45.py
@@ -1,7 +1,18 @@
-#ตรวจจับขอบภาพด้วย Canny Method basic45
+#ตรวจจับขอบภาพด้วย Canny Method  ** ได้รับความนิยมมาก basic45
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread("image/coins.jpg",0)#อ่านภาพ
-img =cv2.resize(img,(500,350))#ปรับขนาดภาพ
+img = cv2.imread("image/coinsbritish.jpg",0)#อ่านภาพ
+img =cv2.resize(img,(700,550))#ปรับขนาดภาพ
+
+#ใช้ในการตรวจจับขอบภาพด้วย Canny Method
+canny = cv2.Canny(img,100,200)
+#cv2.Canny (array รูปภาพ,ค่าของ threshold1,ค่าของ threshold2) ,ค่า threshold1 คือค่าของขอบภาพแรก ,ค่า threshold2 คือค่าของขอบภาพที่สอง
+#ค่า threshold1,2 เราจะกำหนดเอง
+
+cv2.imshow("Original",img)#แสดงภาพ, "Original" คือชื่อภาพตัวแรก, img คือภาพที่เราอ่านมา
+cv2.imshow("Canny",canny)#แสดงภาพ, "Original" คือชื่อภาพตัวแรก , "Canny" คือชื่อภาพตัวที่สอง
+
+cv2.waitKey(0)#รอให้ผู้ใช้งานกดปุ่มเพื่อปิดหน้าต่าง
+cv2.destroyAllWindows()#ปิดหน้าต่าง
